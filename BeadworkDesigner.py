59f74747c0dfe9fb6da89e8259b71905ba73e2a4
Tom (Surface) <t.austinbennett@gmail.com>
2022-04-13 13:50:04 -0400

    init MainWindow

--- BeadworkDesigner.py
+++ BeadworkDesigner.py
@@ -9,9 +9,28 @@ TODO: what goes in a package summary?
 import logging
 import sys
 
+from PyQt6.QtWidgets import QApplication, QMainWindow
+
+
+class MainWindow(QMainWindow):
+
+    def __init__(self):
+        super(MainWindow, self).__init__()
+
+        self.setWindowTitle("Beadwork Designer")
 
 if __name__ == "__main__":
 
     logging.basicConfig(stream = sys.stdout, level=logging.DEBUG)
 
-    logging.info("Starting")
+    logging.info("Starting...")
+
+    app = QApplication(sys.argv)
+
+    window = MainWindow()
+
+    window.show()
+
+    logging.info("Running app")
+
+    app.exec()
