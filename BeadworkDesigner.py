2a51f3873387ef368c4a5d755e5bee4a8028d1fa
Tom (Surface) <t.austinbennett@gmail.com>
2022-04-13 14:47:42 -0400

    init main window

--- BeadworkDesigner.py
+++ BeadworkDesigner.py
@@ -8,14 +8,17 @@ TODO: what goes in a package summary?
 
 import logging
 import sys
+from typing import Container
 
 from PyQt6.QtCore import QSize
 from PyQt6.QtGui import QAction
 from PyQt6.QtWidgets import (
     QApplication, 
+    QHBoxLayout,
     QLabel,
     QMainWindow,
-    QToolBar
+    QToolBar,
+    QWidget
 )
 
 class MainWindow(QMainWindow):
@@ -30,6 +33,8 @@ class MainWindow(QMainWindow):
         self.create_menus()
 
         self.create_toolbar()
+
+        self.create_main_window()
 
         self.setMinimumSize(QSize(1024, 600))
 
@@ -60,9 +65,31 @@ class MainWindow(QMainWindow):
     def create_toolbar(self):
         """Create toolbar for main window.
         """
+        # TODO: add help tips for toolbar
         toolbar = QToolBar("Main Toolbar")
         toolbar.addWidget(QLabel("Toolbar"))
         self.addToolBar(toolbar)
+
+    def create_main_window(self):
+        """Creates main viewing window and widgets
+        """
+
+        # TODO: determine width to set this widget too
+        # TODO: actually make this the widget it should be
+        bead_and_pallete_picker = QLabel("Bead and Pallete Picker")
+
+        # TODO: change background to white
+        editting_window = QLabel("Editting Window")
+
+        layout = QHBoxLayout()
+
+        layout.addWidget(bead_and_pallete_picker)
+        layout.addWidget(editting_window)
+
+        container = QWidget()
+        container.setLayout(layout)
+
+        self.setCentralWidget(container)
 
 if __name__ == "__main__":
 
