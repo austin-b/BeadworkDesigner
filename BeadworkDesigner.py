d3c20c348540655eb4835db0f3a8d6f4441267b0
Tom (Surface) <t.austinbennett@gmail.com>
2022-04-13 13:38:13 -0400

    init BeadworkDesigner.py

---
+++ BeadworkDesigner.py
@@ -0,0 +1,17 @@
+"""
+Beadwork Designer by austin-b
+
+TODO: determine license
+TODO: what goes in a package summary?
+"""
+
+
+import logging
+import sys
+
+
+if __name__ == "__main__":
+
+    logging.basicConfig(stream = sys.stdout, level=logging.DEBUG)
+
+    logging.info("Starting")
