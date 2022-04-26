fa36c4cee1432fd5740ba7c0ee6d19288a9e3b2d
Tom (Surface) <t.austinbennett@gmail.com>
2022-04-26 16:13:50 -0400

    Color Widget for testing

--- EdittingWindow.py
+++ EdittingWindow.py
@@ -5,7 +5,18 @@
 
 import logging
 
-from PyQt6.QtWidgets import QGridLayout, QLabel
+from PyQt6.QtGui import QColor, QPalette
+from PyQt6.QtWidgets import QGridLayout, QWidget
+
+### TODO: FOR TEST PURPOSES ONLY, DELETE WHEN FINISHED
+class Color(QWidget):
+    def __init__(self, color):
+        super().__init__()
+        self.setAutoFillBackground(True)
+
+        palette = self.palette()
+        palette.setColor(QPalette.ColorRole.Window, QColor(color))
+        self.setPalette(palette)
 
 class EdittingWindow(QGridLayout):
     """Layout for editting the actual bead design.
