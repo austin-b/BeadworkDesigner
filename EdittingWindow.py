9372963269ef563dd240043677b410c4e50e7b88
Tom (Surface) <t.austinbennett@gmail.com>
2022-04-26 17:18:15 -0400

    trying to figure out background color for grid

--- EdittingWindow.py
+++ EdittingWindow.py
@@ -1,12 +1,15 @@
 ###
 # TODO: inherit from QGridLayout
 # TODO: populates with default bead widgets until read from file (ie, default model)
+# TODO: add logging functionality
 ###
 
 import logging
 
 from PyQt6.QtGui import QColor, QPalette
-from PyQt6.QtWidgets import QGridLayout, QWidget
+from PyQt6.QtWidgets import QGridLayout, QWidget, QScrollArea
+
+log = logging.getLogger(__name__)
 
 ### TODO: FOR TEST PURPOSES ONLY, DELETE WHEN FINISHED
 class Color(QWidget):
@@ -17,12 +20,30 @@ class Color(QWidget):
         palette = self.palette()
         palette.setColor(QPalette.ColorRole.Window, QColor(color))
         self.setPalette(palette)
+        self.setFixedHeight(500)
 
-class EdittingWindow(QGridLayout):
+class Beadwork(QWidget):
+    def __init__(self):
+        super(QWidget, self).__init__()
+
+        test_grid = QGridLayout()
+
+        for i in range(0,3):
+            test_grid.addWidget(Color("red"), i, 0)
+            test_grid.addWidget(Color("blue"), i, 1)
+            test_grid.addWidget(Color("green"), i, 2)
+            #test_grid.addWidget(Color("purple"), i, 3)
+
+        self.setLayout(test_grid)
+
+class EdittingWindow(QScrollArea):
     """Layout for editting the actual bead design.
     """
 
     def __init__(self):
-        super(QGridLayout, self).__init__()
+        super(QWidget, self).__init__()
 
-        test = QLabel("Editting Window")
+        test_beadwork = Beadwork()
+
+        self.setWidget(test_beadwork)
+        self.setWidgetResizable(True)
