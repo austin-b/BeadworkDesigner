b0e2b5466130beaa85e156042df0ed42e1c8f101
austin-b <t.austinbennett@gmail.com>
2024-02-16 22:43:06 -0500

    test_BeadworkModel_init_nodata

--- tests/test_BeadworkModel.py
+++ tests/test_BeadworkModel.py
@@ -1,5 +1,17 @@
 from BeadworkDesigner.BeadworkModel import BeadworkModel, BeadworkTransposeModel
 
-def test_BeadworkModel():
-    # test_BeadworkModel
-    assert(BeadworkModel() != None)
+# TODO: create test for BeadworkModel init with data
+
+def test_BeadworkModel_init_nodata():
+    testModel = BeadworkModel()
+
+    # TEST FOR NOT EMPTY
+    assert(len(testModel._data) != 0)
+    assert(len(testModel._data[0]) != 0)
+
+    # TEST FOR WHITE DEFAULT WHEN NOT DEBUG
+    assert(data == "#FFFFFF" for row in testModel._data for data in row)
+
+    # TEST FOR RANDOM COLOR DEFAULT WHEN DEBUG
+    testModel = BeadworkModel(debug=True)
+    assert(data != "#FFFFFF" for row in testModel._data for data in row)
