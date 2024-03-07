266d08843dcf8dca0d1d92bd6556ff53abef4e2d
austin-b <t.austinbennett@gmail.com>
2024-03-07 12:55:11 -0500

    added cli argparser and loaded configs from example json project

--- BeadworkDesigner.py
+++ BeadworkDesigner.py
@@ -1,13 +1,23 @@
 import sys
 import logging
+import argparse
 
 from PySide6.QtWidgets import QApplication
 
 from BeadworkDesigner.MainWindow import MainWindow
+from BeadworkDesigner.utils import loadProject
 
 from bin.config import configs  # import default config file
 
-debug = ("--debug" in sys.argv) or configs["debug"]  # check if debug flag is set
+# TODO: provide better description for argparser
+parser = argparse.ArgumentParser(description="Beadwork Designer")
+parser.add_argument("--debug", help="Enable debug mode", action="store_true")
+parser.add_argument("--log", help="Log file", type=str, default=None)
+parser.add_argument("--load", help="Load project", type=str, default=None)
+
+args = parser.parse_args()
+
+debug = (args.debug) or configs["debug"]  # check if debug flag is set
 
 # TODO: implement CLI argument for logging file
 # TODO: make default a log file
@@ -20,7 +30,18 @@ logging.basicConfig(stream = sys.stdout, level=(logging.DEBUG if debug else logg
 logging.info("Starting...")
 
 app = QApplication(sys.argv)
+
+if args.load:
+    json = loadProject(args.load)
+    for key in json['configs'].keys():
+        configs[key] = json['configs'][key]                 # replace any config with the loaded one
+    configs['defaultWidth'] = json['configs']['width']      # override default width
+    configs['defaultHeight'] = json['configs']['height']    # override default height
+
 window = MainWindow(debug=debug, configs=configs)  # check if debug flag is set
+
+if args.load: window.origModel.importData(json['project'])  # dirty, replace with a MainWindow method
+
 window.show()
 
 logging.info("Executing...")
