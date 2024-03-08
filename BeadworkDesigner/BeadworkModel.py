6ef5a3503f6ea172e90e7eb1e66337bebba84b9b
austin-b <t.austinbennett@gmail.com>
2024-03-08 11:57:58 -0500

    todos

--- BeadworkDesigner/BeadworkModel.py
+++ BeadworkDesigner/BeadworkModel.py
@@ -3,12 +3,6 @@ import logging
 from PySide6 import QtCore, QtGui
 from PySide6.QtCore import Qt, QTransposeProxyModel
 
-#####################
-#
-# TODO: create methods for saving and loading to file
-#
-#####################
-
 ### FOR DEBUGGING ###
 def generate_random_color():
     import random
--- BeadworkDesigner/MainWindow.py
+++ BeadworkDesigner/MainWindow.py
@@ -6,7 +6,6 @@ Icons are provided by https://p.yusukekamiyamane.com/. They are licensed under a
 #
 # TODO: change from RGB hex to HSV
 # TODO: add docstrings
-# TODO: add save and load options to file menu
 # TODO: add a way to change the size of the beads
 # TODO: add a way to zoom in and out
 # TODO: add a way to change the color of the background
